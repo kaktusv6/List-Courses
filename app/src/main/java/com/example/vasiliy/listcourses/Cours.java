464c981b4dfd31cafad783cd42e7477a01955f0e
Vasiliy <kaktusv6@gmail.com>
2016-05-04 14:44:43 +1000

    Add class Course in my project. Define field in class Course.

---
+++ app/src/main/java/com/example/vasiliy/listcourses/Cours.java
@@ -0,0 +1,17 @@
+package com.example.vasiliy.listcourses;
+
+/**
+ * Created by vasiliy on 04.05.16.
+ */
+public class Cours {
+    private final String mNameCourse = "";
+    private final String mTypeCourse = "";
+    private final String mTeacherCourse = "";
+    private final String mLectureHallCourse = "";
+
+    public Cours(   String nameCourse, String typeCourse,
+                    String teacherCourse, String lectureHallCourse)
+    {
+
+    }
+}
