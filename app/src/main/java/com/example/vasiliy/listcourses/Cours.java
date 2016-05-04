a7310a42027e7fadd70faf85f561609f20521b8c
Vasiliy <kaktusv6@gmail.com>
2016-05-04 14:46:43 +1000

    Define constructor class Course.

--- app/src/main/java/com/example/vasiliy/listcourses/Cours.java
+++ app/src/main/java/com/example/vasiliy/listcourses/Cours.java
@@ -4,14 +4,19 @@ package com.example.vasiliy.listcourses;
  * Created by vasiliy on 04.05.16.
  */
 public class Cours {
-    private final String mNameCourse = "";
-    private final String mTypeCourse = "";
-    private final String mTeacherCourse = "";
-    private final String mLectureHallCourse = "";
+    private String mNameCourse = "";
+    private String mTypeCourse = "";
+    private String mTeacherCourse = "";
+    private String mLectureHallCourse = "";
 
     public Cours(   String nameCourse, String typeCourse,
                     String teacherCourse, String lectureHallCourse)
     {
-
+        this.mNameCourse = nameCourse;
+        this.mTypeCourse = typeCourse;
+        this.mTeacherCourse = teacherCourse;
+        this.mLectureHallCourse = lectureHallCourse;
     }
+
+
 }
